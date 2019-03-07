8bf696ae594f8cf42601306db8cfd5b221ce6203
Parallels <sam.i.peka@gmail.com>
2019-03-07 21:46:12 +0200

    Add describe functions

--- elf.c
+++ elf.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
@@ -6,7 +7,24 @@
 #include "elftype.h"
 
 
-Elf_ErrNo Elf_validate_elf_header(Elf32_Ehdr * elf_hdr) {
+Elf32_Ehdr * Elf_read_elf_header(int fd) {
+    if (lseek(fd, 0, SEEK_SET) == -1) {
+        return NULL;
+    }
+
+    Elf32_Ehdr * elf_hdr = calloc(1, sizeof(Elf32_Ehdr));
+
+    if (read(fd, elf_hdr, sizeof(elf_hdr)) == -1) {
+        free(elf_hdr);
+        return NULL;
+    }
+
+    return elf_hdr;
+}
+
+
+Elf_ErrNo Elf_validate_elf_header(Elf32_Ehdr * elf_hdr) 
+{
     char * e_ident = elf_hdr->e_ident;
 
     if (strncmp(e_ident, "\x7f""ELF", 4) != 0) {
@@ -25,18 +43,59 @@ Elf_ErrNo Elf_validate_elf_header(Elf32_Ehdr * elf_hdr) {
 }
 
 
-
-Elf32_Ehdr * Elf_read_elf_header(int fd) {
-    if (lseek(fd, 0, SEEK_SET) == -1) {
-        return NULL;
+char * Elf_describe_e_type(Elf32_Half e_type) {
+    switch (e_type) {
+        case ET_NONE:
+            return "No file type";
+        case ET_REL:
+            return "Relocatable file";
+        case ET_EXEC:
+            return "Executable file";
+        case ET_DYN:
+            return "Shared object file";
+        case ET_CORE:
+            return "Core file";
     }
 
-    Elf32_Ehdr * elf_hdr = calloc(1, sizeof(Elf32_Ehdr));
-
-    if (read(fd, elf_hdr, sizeof(elf_hdr)) == -1) {
-        free(elf_hdr);
-        return NULL;
+    if (e_type >= ET_LOPROC) {
+        return "Processor-specific";
     }
 
-    return elf_hdr;
+    return "Unknown";
+}
+
+char * Elf_describe_e_machine(Elf32_Half e_machine) {
+    switch (e_machine) {
+        case EM_NONE:
+            return "No machine";
+        case EM_M32:
+            return "AT&T WE 32100";
+        case EM_SPARC:
+            return "SPARC";
+        case EM_386:
+            return "Intel 80386";
+        case EM_68K:
+            return "Motorola 68000";
+        case EM_88K:
+            return "Motorola 88000";
+        case EM_860:
+            return "Intel 80860";
+        case EM_MIPS:
+            return "MIPS RS3000";
+        default:
+            return "Unknown";
+    }
+}
+
+char * Elf_describe_ei_class(uint8_t ei_class) {
+    switch (ei_class) {
+        case ELFCLASSNONE:
+            return "Invalid class";
+        case ELFCLASS32:
+            return "32-bit objects";
+        case ELFCLASS64:
+            return "64-bit objects";
+        default:
+            return "Unknown";
+    }
 }
